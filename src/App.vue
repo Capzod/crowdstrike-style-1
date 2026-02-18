3d80e58017eec905a2760b00d2c210f899d6b242
Vamsi <Vamsi.krishnan@eigensecure.com>
2026-02-18 20:47:53 +0530

    updated files

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,14 @@
 <template>
   <v-app>
-    <!-- <MainLayout /> -->
-<MainLayout>
+    <MainLayout>
       <router-view v-slot="{ Component }">
         <component :is="Component || ComingSoon" />
       </router-view>
     </MainLayout>
-    <!-- <div v-if="state.isAuthenticated">
-      <div>Welcome, {{ state.user?.name }}!</div>
-      <button @click="handleLogout">Log Out</button>
-    </div>
-
-    <div v-else>
-      <button @click="handleLogin">Log In</button>
-    </div> -->
-
   </v-app>
 </template>
 
-<script setup lang="ts">
-import { onMounted } from 'vue'
-import { useAuth } from './config/useAuth'
-import { myMSALObj, state } from './config/msalConfig'
+<script setup>
 import MainLayout from './layouts/MainLayout.vue'
 import ComingSoon from './views/ComingSoon.vue'
-
-
-const { login, logout, handleRedirect } = useAuth()
-
-const handleLogin = async () => {
-  await login()
-}
-
-const handleLogout = () => {
-  logout()
-}
-
-// const initialize = async () => {
-//   try {
-//     await myMSALObj.initialize()
-//   } catch (error) {
-//     console.log('Initialization error', error)
-//   }
-// }
-
-onMounted(async () => {
-  // await initialize()
-  await handleRedirect()
-})
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import fs from 'fs'
-
 
 export default defineConfig({
   plugins: [vue()],
@@ -10,12 +8,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem')),
-    },
-    port: 5173
   }
 })
