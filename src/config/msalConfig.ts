28f6483d15b57650f6a938cf7fca69cb072bae28
Vamsi <Vamsi.krishnan@eigensecure.com>
2026-02-18 20:14:24 +0530

    Updated files

--- src/config/msalConfig.ts
+++ src/config/msalConfig.ts
@@ -3,15 +3,16 @@
 
  export const msalConfig = {
  auth: {
-//  clientId: 'd12df531-8e2c-4d85-ac14-ab56707004c6',
-//  authority: 'https://login.microsoftonline.com/1e499b00-b34d-4461-b219-a45d602ca97c',
-//  redirectUri: 'https://localhost:5173/', // Replace with your actual redirect URI
-//  postLogoutUri: 'https://localhost:5173/'
-  clientId: import.meta.env.VITE_AZURE_CLIENT_ID,
-    authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_TENANT_ID}`,
-    redirectUri: import.meta.env.VITE_REDIRECT_URI,
-    postLogoutRedirectUri: import.meta.env.VITE_REDIRECT_URI
+  clientId: 'd12df531-8e2c-4d85-ac14-ab56707004c6',
+  authority: 'https://login.microsoftonline.com/1e499b00-b34d-4461-b219-a45d602ca97c',
+  redirectUri: 'https://crowdstrike-style-1.vercel.app/', // Replace with your actual redirect URI
+  postLogoutUri: 'https://crowdstrike-style-1.vercel.app/'
+//  clientId: import.meta.env.VITE_AZURE_CLIENT_ID,
+  //  authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_TENANT_ID}`,
+    //redirectUri: import.meta.env.VITE_REDIRECT_URI,
+    // postLogoutRedirectUri: import.meta.env.VITE_REDIRECT_URI
  },
+ 
  cache: {
  cacheLocation: 'sessionStorage', // This configures where your cache will be stored
  storeAuthStateInCookie: false
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem')),
+    },
+    port: 5173
   }
 })
